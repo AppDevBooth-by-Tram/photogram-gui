9501ff9f4e7ad30e4974e9fd18248de39e940a6a
tramdoman <tramdoman@chicagobooth.edu>
2022-02-22 03:49:43 +0000

    photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,4 +3,30 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all.order({ :created_at => "desc" })
     render({ :template => "photos_templates/index.html.erb"})
   end
+
+  def show
+    @the_photo = Photo.where({ :id => params.fetch("path_id")}).first
+    render({ :template => "photos_templates/show.html.erb"})
+  end
+
+  def delete
+    matching_photos = Photo.where({ :id => params.fetch("destroy_id")})
+    the_photo = matching_photos.at(0)
+    the_photo.destroy
+    #render({ :template => "photos_templates/gone.html.erb"})
+    redirect_to("/photos")
+  end
+  
+  def create
+  input_image = params.fetch("input_image")
+  input_caption = params.fetch("input_caption")
+  input_owner_id = params.fetch("input_owner_id")
+  a_new_photo = Photo.new
+  a_new_photo.image = input_image
+  a_new_photo.caption = input_caption
+  a_new_photo.owner_id = input_owner_id
+  a_new_photo.save
+    redirect_to("/photos/" + a_new_photo.id.to_s)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   get("/users/:path_username", { :controller => "users", :action => "show"})
 
   get("/photos", { :controller => "photos", :action => "index"})
+
+  get("/photos/:path_id", { :controller => "photos", :action => "show"})
+
+  get("/delete_photo/:destroy_id", { :controller => "photos", :action => "delete"})
+  
+  get("/insert_photo", { :controller => "photos", :action => "create"})
+
 end
